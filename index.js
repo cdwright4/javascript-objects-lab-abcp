6c5eb1c1018519db4021e313d1eeb349b45c3b43
Crystal <crystal_wright@yahoo.com>
2018-01-15 19:15:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey (object, key) {
   
-  var newObj = Object.assign(object);
-  newObj.delete[key];
+  var newObj = Object.assign({}, object);
+  delete newObj.[key];
   return newObj;
   
 }
